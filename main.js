6e535064df57b6ee1ffc7f1205efcbc24fbac54e
Krishnaveni <veniumabala@gmail.com>
2022-12-16 08:40:52 +0000

    add

---
+++ main.js
@@ -0,0 +1,13 @@
+let firstname=prompt("Enter the Name");
+let num=prompt("How many times print the Time");
+let i=1;
+function fun()
+{
+    for(i=1;i<=num;i++)
+    {
+        document.write(firstname);
+        document.write("<br>");
+    }
+}
+fun();
+fun();
